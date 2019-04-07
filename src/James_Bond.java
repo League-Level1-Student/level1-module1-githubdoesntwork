d315ed5bbffdc54e81c78c22673348fcbad44f65
Michael Lee <NewImac@unknown78321bc19d40.attlocal.net>
2019-04-06 18:32:30 -0700

    finished jmes bond

--- src/James_Bond.java
+++ src/James_Bond.java
@@ -1,14 +1,21 @@
 
 public class James_Bond {
 	public static void main(String[] args) {
-		findCode();
+		//put "findCode();" here :)
 	}
 
 	static Vault vault = new Vault(932143);
 
 	public static void findCode() {
 		for (int i = 0; i < 1000000; i++) {
-			vault.tryCode(i);
+			boolean unlocked = vault.tryCode(i);
+			if (unlocked) {
+				System.out.println("The vault is open.");
+				break;
+			} if(!unlocked){
+				System.out.println("The vault is not open");
+	
+			}
 		}
 	}
 }
